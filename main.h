67440f3ca5e4e6970a9681129b80dd453edcffbd
TheKasman <kasapidisandrew@gmail.com>
2025-11-18 10:11:30 +1100

    to the bit

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+
+void printString(); /*These will probably take in the ... based on a switch case*/
+void printChar();
+void printPercent();
+
+
+#endif
