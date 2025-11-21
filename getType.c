c29d841b0a426b0521adc3c2d0bf6de9b1c74253
TheKasman <kasapidisandrew@gmail.com>
2025-11-21 15:52:32 +1100

    forgot to add it to getType

--- getType.c
+++ getType.c
@@ -16,24 +16,19 @@ int getType(char letter, char *buff, va_list args)
 		case 'c':
 			buff[0] = (char)va_arg(args, int);
 			return (1);
-		case 's':
-			return (printString(buff, args));
+		case 's': return (printString(buff, args));
 		case '%':
 			buff[0] = '%';
 			return (1);
 		case 'd':
 		case 'i':
 			return (printInt(buff, args));
-		case 'b':
-			return (printBin(buff, args));
-		case 'u':
-			return (printUInt(buff, args));
-		case 'o':
-			return (printOct(buff, args));
-		case 'x':
-			return (print_hex(buff, args));
-		case 'X':
-			return (print_HEX(buff, args));
+		case 'b': return (printBin(buff, args));
+		case 'u': return (printUInt(buff, args));
+		case 'o': return (printOct(buff, args));
+		case 'x': return (print_hex(buff, args));
+		case 'X': return (print_HEX(buff, args));
+		case 'S': return (printSuper(buff, args));
 		default:
 			buff[0] = '%';
 			buff[1] = letter;
